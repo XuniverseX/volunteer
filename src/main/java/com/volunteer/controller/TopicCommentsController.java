b4a69a399f1b1debf003c4ff09f4ee4efc98bb23
xuni <1029805801@qq.com>
2022-05-31 17:56:20 +0800

    评论保存

--- src/main/java/com/volunteer/controller/TopicCommentsController.java
+++ src/main/java/com/volunteer/controller/TopicCommentsController.java
@@ -1,8 +1,15 @@
 package com.volunteer.controller;
 
+import com.volunteer.dto.Result;
+import com.volunteer.entity.TopicComments;
+import com.volunteer.service.ITopicCommentsService;
+import com.volunteer.utils.UserHolder;
 import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  *
@@ -13,4 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/topic-comments")
 @Api("话题评论操作")
 public class TopicCommentsController {
+
+    @Resource
+    private ITopicCommentsService topicCommentsService;
+
+    @PostMapping("/save")
+    public Result saveTopicComments(TopicComments topicComments) {
+        String openid = UserHolder.getUser().getOpenid();
+        topicComments.setUserId(openid);
+        topicCommentsService.save(topicComments);
+        return Result.ok(topicComments.getId());
+    }
+
 }
--- src/main/java/com/volunteer/entity/TopicComments.java
+++ src/main/java/com/volunteer/entity/TopicComments.java
@@ -27,7 +27,7 @@ public class TopicComments implements Serializable {
     /**
      * 用户id
      */
-    private Long userId;
+    private String userId;
 
     /**
      * 探店id
