1f95a59a03f71c8131fef999b2c4e64f416429a0
xuni <1029805801@qq.com>
2022-06-24 15:50:14 +0800

    Recruit招募接口添加

--- src/main/java/com/volunteer/utils/RedisConstants.java
+++ src/main/java/com/volunteer/utils/RedisConstants.java
@@ -1,7 +1,7 @@
 package com.volunteer.utils;
 
 public class RedisConstants {
-    public static final String LOGIN_USER_KEY = "login:token:";
+    public static final String LOGIN_USER_KEY = "login:wechat:token:";
     public static final Long LOGIN_USER_TTL = 30L;
     public static final String TOPIC_LIKED_KEY = "topic:liked:";
 }
